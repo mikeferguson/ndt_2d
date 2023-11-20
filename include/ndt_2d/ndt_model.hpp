6a1157ce6691424f96fb970dff94bbfcecb3ef69
Michael Ferguson <mfergs7@gmail.com>
2023-11-20 15:46:46 -0500

    new constructors for Pose2d

--- include/ndt_2d/ndt_model.hpp
+++ include/ndt_2d/ndt_model.hpp
@@ -24,6 +24,20 @@ struct Point
 
 struct Pose2d
 {
+  Pose2d()
+  {
+    x = 0.0;
+    y = 0.0;
+    theta = 0.0;
+  }
+
+  Pose2d(double x, double y, double theta)
+  {
+    this->x = x;
+    this->y = y;
+    this->theta = theta;
+  }
+
   double x, y, theta;
 }l;
 
